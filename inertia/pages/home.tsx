eb8122c5bfb4a1f5c3217a38e212c5ed17bcea69
Moumouuu <robin.travail007@gmail.com>
2024-05-19 19:37:40 +0200

    feat(): config shadncn ui

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,11 +1,13 @@
 import { Head } from '@inertiajs/react'
+import { Button } from "~/components/ui/button";
 
 export default function Home() {
   return (
     <>
       <Head title="Homepage" />
 
-      <h1 className={""}>Homepage</h1>
+      <h1 className={"text-red-500"}>Homepage</h1>
+      <Button variant={"default"}>Click me</Button>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { getDirname } from '@adonisjs/core/helpers'
 import inertia from '@adonisjs/inertia/client'
 import react from '@vitejs/plugin-react'
 import adonisjs from '@adonisjs/vite/client'
+import path from "path"
 
 export default defineConfig({
   plugins: [inertia({ ssr: { enabled: false } }), react(), adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] })],
@@ -13,6 +14,7 @@ export default defineConfig({
    */
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "./src"),
       '~/': `${getDirname(import.meta.url)}/inertia/`,
     },
   },
