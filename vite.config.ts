95bcff7f739b3912c712c8533ca0ea8b42714922
Moumouuu <robin.travail007@gmail.com>
2024-05-20 13:51:21 +0200

    feat(home page): desktop header add assets & shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import { getDirname } from '@adonisjs/core/helpers'
 import inertia from '@adonisjs/inertia/client'
 import react from '@vitejs/plugin-react'
 import adonisjs from '@adonisjs/vite/client'
-import path from "path"
+import path from 'node:path'
 
 export default defineConfig({
-  plugins: [inertia({ ssr: { enabled: false } }), react(), adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] })],
+  plugins: [
+    inertia({ ssr: { enabled: false } }),
+    react(),
+    adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] }),
+  ],
 
   /**
    * Define aliases for importing modules from
@@ -14,7 +18,7 @@ export default defineConfig({
    */
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
       '~/': `${getDirname(import.meta.url)}/inertia/`,
     },
   },
