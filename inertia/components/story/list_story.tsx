6ea704c37581a5694bacb489fd0011d6f460e25b
Moumouuu <robin.travail007@gmail.com>
2024-05-23 17:50:46 +0200

    fix(ui):

--- inertia/components/story/list_story.tsx
+++ inertia/components/story/list_story.tsx
@@ -7,7 +7,7 @@ import Story from '#models/story'
 export function ListStory({ stories }: InferPageProps<IndexStoryController, 'index'>) {
   return (
     <>
-      <div className={'grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 '}>
+      <div className={'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '}>
         {stories?.map((story) => (
           <DrawerDialog
             key={story.id}
