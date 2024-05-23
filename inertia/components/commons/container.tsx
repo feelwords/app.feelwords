d57ef35a0b95464e04ff3563f6900fabef5a6315
Moumouuu <robin.travail007@gmail.com>
2024-05-23 17:50:41 +0200

    fix(ui):

--- inertia/components/commons/container.tsx
+++ inertia/components/commons/container.tsx
@@ -1,6 +1,6 @@
 export function Container({ children }: { children: React.ReactNode }) {
   return (
-    <section className={'p-3 md:p-4 lg:p-10 flex justify-center'}>
+    <section className={'p-3 md:p-4 lg:p-10 mb-20 flex justify-center'}>
       <div className={'max-w-7xl w-full'}>{children}</div>
     </section>
   )
